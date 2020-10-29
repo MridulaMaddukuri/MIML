5bdde49ede23d2fb66621be008b20a1273cef3eb
Mridula Maddukuri <mridulamaddukuri@utexas.edu>
2020-10-29 11:16:27 -0700

    changed the name of the package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="WIML", # Replace with your own username
+    name="MIML", # Replace with your own username
     version="0.0.1",
     author="Mridula Maddukuri",
     author_email="mridulamaddukuri@utexas.edu",
